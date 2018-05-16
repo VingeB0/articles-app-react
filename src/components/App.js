58044ac03db68f8ef90a91cc4363931d589720dd
Vladymyr Davii <vladimir.davii@gmail.com>
2018-05-16 13:16:31 +0300

    control input

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,22 @@ class App extends Component {
 
     };
 
+    state = {
+      inputValue: ''
+    };
+
+    handleInputValueChange = (ev) => {
+        this.setState({
+            inputValue: ev.target.value
+        });
+    };
+
     render() {
+        console.log(this.state.inputValue);
+
         return (
             <div>
+                Name <input type="text" value={this.state.text} onChange={this.handleInputValueChange} />
                 <ArticleList articles={ this.props.articles } />
                 <ArticleChart articles={ this.props.articles } />
             </div>
