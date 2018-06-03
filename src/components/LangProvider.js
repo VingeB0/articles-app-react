c8a9d63116c902f6fafb981906aebe54fb72cab0
Vladymyr Davii <vladimir.davii@gmail.com>
2018-06-03 22:55:27 +0300

    fix

--- src/components/LangProvider.js
+++ src/components/LangProvider.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import dictionaries from '../dictionaries'
+import dictionaries from './dictionaries'
 
 class LangProvider extends Component {
     static propTypes = {
